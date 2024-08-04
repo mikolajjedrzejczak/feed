2f9864580b8ac7cb69061845860586d7cbed7337
MKJ <mik.jed2003@gmail.com>
2024-08-04 10:30:21 +0200

    fix: update proxy target

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://jsonplaceholder.typicode.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
